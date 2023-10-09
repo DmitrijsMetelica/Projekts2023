1b7bc3a8bb1b48b5e5e6552906e4194b69a1b4e3
DmitrijsMetelica <dmitrijs.metelica@marupe.edu.lv>
2023-10-09 15:26:37 +0300

    UZ DB

--- proc.php
+++ proc.php
@@ -1,7 +1,16 @@
 <?php
 
 $l = $_POST["login"];
-$p = $a = $_POST['password'];
-echo "$l, HELLO, $p";
+$p = $_POST['password'];
+
+include("cfg.php");
+$mysqli = mysqli_query("SELECT * FROM users WHERE (login = '$l') AND (password = '$p')")
+if (mysqli_num_rows($result) > 0){
+    echo "GOOOOOD!!!!";
+}
+else{
+    echo "BYE BYE";
+}
+
 
 ?>
